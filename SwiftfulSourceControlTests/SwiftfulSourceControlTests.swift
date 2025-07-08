6d7cb6a3400d99795513428f2b7b5559fbd02b92
oguzhanerbas5 <oguzhanerbas8@gmail.com>
2025-07-08 12:39:14 +0300

    Initial Commit

---
+++ SwiftfulSourceControlTests/SwiftfulSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlTests.swift
+//  SwiftfulSourceControlTests
+//
+//  Created by eoguzhan on 8.07.2025.
+//
+
+import Testing
+@testable import SwiftfulSourceControl
+
+struct SwiftfulSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
